076e1fab26fd21427612683d7b3379ae355cbfd6
Ryan Milton <mr.ryan.milton@gmail.com>
2023-01-31 19:56:45 -0800

    Minor changes

--- src/Components/SIgnUp.jsx
+++ src/Components/SIgnUp.jsx
@@ -5,7 +5,8 @@ export default function SignUp(props) {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
 
-  const signUpRequest = () => {
+  const signUpRequest = (event) => {
+    event.preventDefault();
     alert('Sign Up is currently disabled. Please try again later. We apologize for any inconvenience this may have caused you.')
   }
 
@@ -14,21 +15,21 @@ export default function SignUp(props) {
       <div ref={scrollRefTwo} className="flex justify-center w-full h-screen bg-layered-waves bg-no-repeat bg-cover">
         <div className="mt-40">
           <div className="flex flex-col items-center">
-            <h1 className="pb-5 text-8xl font-brandmark" >Sign Up</h1>
-            <h1 className="text-8xl font-brandmark" >For Early Access!</h1>
+            <h1 className="pb-5 text-8xl font-brandmark text-eva-text" >Sign Up</h1>
+            <h1 className="text-8xl font-brandmark text-eva-text" >For Early Access!</h1>
           </div>
           <div className="flex mx-auto w-1/2 mt-20 border-4 border-eva-highlight rounded-xl h-fit bg-black drop-shadow-[0_25px_25px_rgb(0,0,0,0.6)]">
             <form className="mx-10">
               <div className="mt-10">
-                <label for="name" className="block text-4xl font-brandmark">Full Name</label>
+                <label for="name" className="block text-4xl font-brandmark text-eva-text">Full Name</label>
                 <input onChange={(e) => setName(e.target.value)} required={true} placeholder="John Doe" type="text" className="pl-2 rounded-lg py-2 w-full my-2 text-2xl font-brandmark outline-none"/>
               </div>
               <div className="mt-10">
-                <label for="email" className="block text-4xl font-brandmark">Email</label>
+                <label for="email" className="block text-4xl font-brandmark text-eva-text">Email</label>
                 <input onChange={(e) => setEmail(e.target.value)} required={true} placeholder="example@gmail.com" type="email" className="pl-2 rounded-lg py-2 w-full my-2 text-2xl font-brandmark outline-none" />
               </div>
               <div className="flex justify-center my-10">
-                <button onClick={() => signUpRequest()} className="text-4xl font-brandmark w-full hover:bg-eva-bg hover:text-eva-text hover:border-eva-highlight" >Sign Up!</button>
+                <button onClick={(e) => signUpRequest(e)} className=" bg-black text-4xl font-brandmark w-full hover:bg-eva-bg hover:text-eva-text hover:border-eva-highlight text-eva-text" >Sign Up!</button>
               </div>
               <p className="flex flex-wrap font-brandmark text-gray-500">When the app becomes available, you will receive an email notifying you to download the app.</p>
               <p className="flex flex-wrap font-brandmark mb-10 text-gray-500">Expected release in Q2 2023.</p>
--- src/Components/iOSDisplay.jsx
+++ src/Components/iOSDisplay.jsx
@@ -7,8 +7,8 @@ export default function IPhoneDisplay(props) {
     <div ref={scrollRefOne} className="flex flex-col w-full h-screen bg-black">
       <div className="flex h-[90%]">
         <div className="flex flex-col w-3/4 justify-center px-20">
-          <h1 className="text-8xl font-brandmark mt-20">Introducing EVA, </h1>
-          <p className=" text-3xl font-brandmark">The cutting-edge AI assistant app powered by OpenAI's GPT-3 and soon GPT-4. EVA is designed to make your life easier by providing you with a personal assistant that can understand your needs, anticipate your requests and complete tasks for you. With EVA, you will have access to the most advanced natural language processing technology available. EVA can understand and respond to your requests in a way that is both efficient and natural, making it feel like you are talking to a real person. EVA uses GPT-4's state-of-the-art machine learning algorithms to learn about you and your preferences, making it more efficient and personalized with every interaction. With EVA, you can focus on what matters most and leave the rest to us. Upgrade your life with EVA today!</p>
+          <h1 className="text-8xl font-brandmark mt-20 text-eva-text">Introducing EVA, </h1>
+          <p className=" text-3xl font-brandmark text-eva-text">The cutting-edge AI assistant app powered by OpenAI's GPT-3 and soon GPT-4. EVA is designed to make your life easier by providing you with a personal assistant that can understand your needs, anticipate your requests and complete tasks for you. With EVA, you will have access to the most advanced natural language processing technology available. EVA can understand and respond to your requests in a way that is both efficient and natural, making it feel like you are talking to a real person. EVA uses OpenAI's state-of-the-art machine learning algorithms to learn about you and your preferences, making it more efficient and personalized with every interaction. With EVA, you can focus on what matters most and leave the rest to us. Upgrade your life with EVA today!</p>
         </div>
         <div className="flex w-1/2 justify-center">
           <img
